343bd4a2f32952534f650217e77767ae7bf61631
Mayumayumau <mayasidorova@yahoo.com>
2021-03-09 23:24:16 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+    n = Math.abs(n);
+    let reverse = 0;
+    let tempArr = [];
+    while (n > 0){
+        tempArr.push(n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    for (let i=0; i < tempArr.length; i++) {
+        reverse = reverse * 10 + tempArr[i];
+    }
+
+
+    return reverse;
   
 }
